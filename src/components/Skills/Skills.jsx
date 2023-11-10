a39bb92865edafef1b7caa63c3a749aae0b21809
Vanessa <vmarieharmon@gmail.com>
2023-11-10 13:43:01 -0600

    added tech stack to skills section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -8,6 +8,7 @@ import react from '../../assets/techstack/react.png';
 import fastapi from '../../assets/techstack/FastAPI.png';
 import postgresql from '../../assets/techstack/postgresql.png';
 import docker from '../../assets/techstack/docker.png';
+import javascript from '../../assets/techstack/javascript.png';
 
 
 const Skills = () => {
@@ -16,13 +17,14 @@ const Skills = () => {
         <span className="skillTitle">Tech stack</span>
         <div className="skillBars">
             <img src={html} alt="html" className="skillBarImg" />
-            <img src={dj} alt="django" className="skillBarImg" />
-            <img src={react} alt="html" className="skillBarImg" />
-            <img src={postgresql} alt="html" className="skillBarImg" />
             <img src={css} alt="css" className="skillBarImg" />
-            <img src={restfulapi} alt="html" className="skillBarImg" />
+            <img src={dj} alt="django" className="skillBarImg" />
+            <img src={javascript} alt="javascript" className="skillBarImg" />
+            <img src={react} alt="react" className="skillBarImg" />
+            <img src={restfulapi} alt="restful api" className="skillBarImg" />
+            <img src={docker} alt="docker" className="skillBarImg" />
             <img src={fastapi} alt="html" className="skillBarImg" />
-            <img src={docker} alt="html" className="skillBarImg" />
+            <img src={postgresql} alt="postgresql" className="skillBarImg" />
         </div>
     </section>
   )
