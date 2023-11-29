d8081b2780f8e6d7c2b347c42efebc25b48c92f0
Vanessa <vmarieharmon@gmail.com>
2023-11-29 13:05:53 -0800

    spelling correction to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './footer.css';
 const footer = () => {
   return (
     <footer className='footer'>
-        Copyright &#169; 2023 Vanessa Harmon. All right received.
+        Copyright &#169; 2023 Vanessa Harmon. All rights reserved.
     </footer>
   )
 }
